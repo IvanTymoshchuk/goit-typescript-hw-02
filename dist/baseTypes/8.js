9d194223fb60a2ca864585c93c68aaaee383428f
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-10-16 16:44:20 +0200

    fisrt tasks

--- dist/baseTypes/8.js
+++ dist/baseTypes/8.js
@@ -2,6 +2,7 @@
   Створіть тип "Gender", використовуючи union type,
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
-const myGender;
-export {};
+const myGender = 'male';
+console.log(myGender);
+export { myGender };
 //# sourceMappingURL=8.js.map
--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -3,6 +3,9 @@
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
 
-const myGender;
+type Gender = "male" | "female"
 
-export {};
+const myGender: Gender = 'male';
+console.log(myGender);
+
+export {  };
