e7a2df90043685da4d3b25d583757b4b0aba9c2d
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-10-16 10:31:59 +0200

    Update 1.ts

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,10 +1,35 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
 
-let age = 50;
-let name = 'Max';
-let toggle = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+let age: number = 50;
+let name: string = 'Max';
+let toggle: boolean = true;
+let empty: null = null;
+let notInitialize: any;
+let callback: (a: number) => number = (a) => { return 100 + a };
 
-export {};
+
+// interface User {
+//   age: number;
+//   name: string;
+//   toggle: boolean;
+//   empty: null;
+//   notInitialize: any;
+//   callback: any;
+// }
+
+// let user: User = {
+//   age: 50,
+//   name: 'Max',
+//   toggle: true,
+//   empty: null,
+//   notInitialize, //* Підсвічує що не зроблена початкова ініціалізація
+//   callback: (a) => { return 100 + a },
+// }
+
+
+
+
+
+
+
+export { };
